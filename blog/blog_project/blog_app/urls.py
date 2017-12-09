937ce90dec6c73c4e9c8275f1bab40bc071abb8d
gourav <gouravacharya19@gmail.com>
2017-12-08 21:48:07 -0500

    Used forms, deleteview

--- blog/blog_project/blog_app/urls.py
+++ blog/blog_project/blog_app/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
+from django.urls import path
+
 
 from . import views
 
 urlpatterns=[url(r'^$',views.BlogListView.as_view(),name='post_list'),
-             url(r'^post/(?P<pk>\d+)/$',views.BlogDetailView.as_view(),name="post_detail")]
+             url(r'^post/(?P<pk>\d+)/$',views.BlogDetailView.as_view(),name="post_detail"),
+             url(r'^post/new/$',views.BlogCreateView.as_view(),name="post_new"),
+             url(r'^post/(?P<pk>\d+)/edit/$',views.BlogUpdateView.as_view(),name="post_edit"),
+             path('post/<int:pk>/delete/',views.BlogDeleteView.as_view(), name='post_delete'),
+             ]
--- blog/blog_project/blog_app/views.py
+++ blog/blog_project/blog_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model=Post
@@ -11,6 +13,21 @@ class BlogDetailView(DetailView):
     model=Post
     template_name='blog_app/post_detail.html'
     context_object_name = 'parsed_object_for_detail_view'
+
+class BlogCreateView(CreateView):
+	model=Post
+	template_name='blog_app/post_new.html'
+	fields='__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title','text']
+    template_name = "blog_app/post_edit.html"
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "blog_app/post_delete.html"
+    success_url = reverse_lazy("post_list")
+
 
 # Create your views here.
 
